94e7b39f2d32dd3e7df996abeb6c6e36907d58d8
Camsca <camila_vass@hotmail.com>
2023-10-08 12:43:24 -0700

    inquirer.prompt to ask defined questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,5 @@
 //packages needed for this application
-const inquirer = require ('inquirer')
+const inquirer = require ('inquirer');
 const fs = require ('fs');
 const { error } = require('console');
 
@@ -79,7 +79,7 @@ const questions = [
     },
 ];
 
-// TODO: Create a function to write README file
+// function to write README file
 function writeTo(fileName, data) {
     fs.writeFile(fileName,data,(error)=>{
         if(error){
@@ -92,8 +92,14 @@ function writeTo(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { 
+    inquirer
+    .prompt(questions)
+    .then((answers)=>{
+
+    })
+}
 
 // Function call to initialize app
-init();
+init(); 
 erdf
